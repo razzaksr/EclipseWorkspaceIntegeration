95b61dfd2483d859eb4ba58c3852e45c0adacaf3
razzaksr <razzaksr@gmail.com>
2020-09-09 18:05:06 +0530

    first

---
+++ DLitheInternCore/src/main/java/internship/dlithe/DLitheInternCore/Beginning.java
@@ -0,0 +1,8 @@
+package internship.dlithe.DLitheInternCore;
+
+public class Beginning 
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to DLithe for the Internship");
+	}
+}
